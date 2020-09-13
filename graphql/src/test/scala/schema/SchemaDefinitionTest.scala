029c2117f3edf5802325a75c87968e08bf97a818
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-09-13 22:15:54 +0200

    Extracted common code in tests to methods.

--- graphql/src/test/scala/schema/SchemaDefinitionTest.scala
+++ graphql/src/test/scala/schema/SchemaDefinitionTest.scala
@@ -7,23 +7,25 @@ import model.User
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import repository.UserRepository
+import sangria.ast.Document
 import sangria.execution.Executor
 import sangria.macros._
 import sangria.marshalling.circe._
+import sangria.schema.Schema
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
 class SchemaDefinitionTest extends AnyWordSpec with Matchers {
-  "SchemaDefinition" when {
-    val schema = SchemaDefinition.schema
-    val userRepository = UserRepository()
+  val schema: Schema[UserRepository, Unit] = SchemaDefinition.schema
+  val userRepository: UserRepository = UserRepository()
 
+  "SchemaDefinition" when {
     "queried for users" should {
-      "return defined users list" in {
-        val query =
-          graphql"""
+
+      val allUsersQuery =
+        graphql"""
             query {
               users {
                 username,
@@ -32,21 +34,17 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
             }
           """
 
-        val future = Executor
-          .execute(
-            schema = schema,
-            queryAst = query,
-            userContext = userRepository
-          )
+      "return defined users list" in {
+
+        val future = query(allUsersQuery)
           .map(parseQueryData[List[User]](_, "users"))
         val users = Await.result(future, 10.seconds)
 
         users should be(userRepository.users)
       }
 
-      "return existing user" in {
-        val query =
-          graphql"""
+      val userQuery =
+        graphql"""
             query($$username: String!) {
               user(username: $$username) {
                 username,
@@ -54,58 +52,37 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
               }
             }
           """
-        val variables = json(
-          """
-            {
-              "username": "Pafeu"
-            }
-          """
-        )
 
-        val future = Executor
-          .execute(
-            schema = schema,
-            queryAst = query,
-            variables = variables,
-            userContext = userRepository
-          )
+      "return existing user" in {
+        val variables = usernameVariables("Pafeu")
+
+        val future = query(userQuery, variables)
           .map(parseQueryData[Option[User]](_, "user"))
         val user = Await.result(future, 10.seconds)
 
         user should be(userRepository.user("Pafeu"))
       }
 
-      "return empty json for nonexistent user" in {
-        val query =
-          graphql"""
-            query($$username: String!) {
-              user(username: $$username) {
-                username,
-                status
-              }
-            }
-          """
-        val variables = json(
-          """
-            {
-              "username": "Not exists"
-            }
-          """
-        )
+      "return null for nonexistent user" in {
+        val variables = usernameVariables("Nonexistent")
 
-        val future = Executor
-          .execute(
-            schema = schema,
-            queryAst = query,
-            variables = variables,
-            userContext = userRepository
-          )
+        val future = query(userQuery, variables)
           .map(parseQueryData[Option[User]](_, "user"))
         val user = Await.result(future, 10.seconds)
 
         user should be(None)
       }
     }
+  }
+
+  private def query(userQuery: Document, variables: Json = Json.obj()) = {
+    Executor
+      .execute(
+        schema = schema,
+        queryAst = userQuery,
+        variables = variables,
+        userContext = userRepository
+      )
   }
 
   private def parseQueryData[T](json: Json, field: String)(implicit
@@ -115,6 +92,16 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
       case Right(value)  => value
       case Left(failure) => throw new RuntimeException(failure.message)
     }
+  }
+
+  private def usernameVariables(username: String) = {
+    json(
+      s"""
+            {
+              "username": "$username"
+            }
+      """
+    )
   }
 
   private def json(string: String) = {
