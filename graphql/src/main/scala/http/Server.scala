2680db185202e7ce384e5dc9be2d0eb032535a6b
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-09-16 21:32:09 +0200

    Implemented GraphQL endpoint and GraphiQL.

--- graphql/src/main/scala/http/Server.scala
+++ graphql/src/main/scala/http/Server.scala
@@ -1,26 +1,41 @@
 package http
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import io.circe.Json
-import sangria.parser.QueryParser
 import io.circe.syntax._
+import repository.UserRepository
 import sangria.ast.Document
+import sangria.execution.Executor
+import sangria.marshalling.circe._
+import sangria.parser.QueryParser
+import schema.SchemaDefinition
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 object Server extends App {
-  implicit val system: ActorSystem = ActorSystem("graphql")
+  implicit val system: ActorSystem                        = ActorSystem("graphql")
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  val userRepository = UserRepository()
 
   val route = path("graphql") {
     post {
-      entity(as[Json]) { request => graphqlEnpoint(request) }
+      entity(as[Json]) { request => graphqlEndpoint(request) }
+    }
+  } ~ path("graphiql") {
+    get {
+      getFromResource("graphiql.html")
     }
   }
 
-  def graphqlEnpoint(request: Json) = {
+  Http().newServerAt("localhost", 8080).bind(route)
+
+  def graphqlEndpoint(request: Json) = {
     val query = request.hcursor
       .downField("query")
       .as[String] match { // TODO: Cleanup code that uses hcursor
@@ -31,7 +46,11 @@ object Server extends App {
       case Success(queryAst) =>
         val operation = request.hcursor.get[String]("operationName").toOption
         val variables =
-          request.hcursor.downField("variables").focus.getOrElse(Json.obj())
+          request.hcursor
+            .downField("variables")
+            .focus
+            .filter(_.toString != "null")
+            .getOrElse(Json.obj())
         complete(executeQuery(queryAst, operation, variables))
       case Failure(exception) =>
         complete(
@@ -45,5 +64,13 @@ object Server extends App {
       queryAst: Document,
       operation: Option[String],
       variables: Json
-  ) = ???
+  ) = {
+    Executor.execute(
+      SchemaDefinition.schema,
+      queryAst,
+      userRepository,
+      variables = variables,
+      operationName = operation
+    )
+  }
 }
