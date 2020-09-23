45a0e59d10c73605c8844aa252f9a6008c579d3e
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-09-23 20:26:37 +0200

    Added basic-graphql docker config. Replaced "localhost" with "0.0.0.0" to listen to all network interfaces.

--- basic-graphql/src/main/scala/http/Server.scala
+++ basic-graphql/src/main/scala/http/Server.scala
@@ -18,7 +18,7 @@ object Server extends App with Config[ServerConfig] with Logging {
   val userRepository = UserRepository()
   val route          = GraphQl(userRepository).route ~ GraphiQl.route
 
-  Http().newServerAt("localhost", config.port).bind(route)
+  Http().newServerAt("0.0.0.0", config.port).bind(route)
   scribe.info("GraphQL server started")
 
   override def defaultConfig = ServerConfig(port = 8080)
