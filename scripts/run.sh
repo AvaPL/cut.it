44e6b9f7a71d94127f83232bd2cfde71508ae60c
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-10-08 23:39:30 +0200

    Fixed scripts according to compose changes.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-# TODO: Add argument for selecting cut.it/infra docker-compose files
-
 cd ../
+
+# Process script arguments
 while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        # TODO: Update with cut-link project
-        -i|--images) echo "Building Docker images ..."; sbt "; project basic-graphql; docker:publishLocal"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
+  case $1 in
+  # TODO: Extract cases to functions
+  -i | --images)
+    echo "Building Docker images ..."
+    sbt "; project cut-link; docker:publishLocal" # TODO: Extract project names to a variable
+    ;;
+  cut.it | infra) profile=$1 ;; # TODO: Extract profiles to a variable
+  *)
+    echo "Unknown parameter passed: $1"
+    exit 1
+    ;;
+  esac
+  shift
 done
-cd docker || exit
+
+# Check specified profile
+if [ -z "$profile" ]; then
+  echo -e "No profile specified, use one of: [cut.it, infra]"
+  exit 1
+fi
+
+# Start containers
+cd docker/"$profile" || exit
 docker-compose up -d
-cd ../scripts || exit
+cd ../../scripts || exit
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# TODO: Use cut.it/infra docker-compose files
-
 cd ../
-cd docker || exit
+
+# Process script arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  # TODO: Extract cases to functions
+  cut.it | infra) profile=$1 ;; # TODO: Extract profiles to a variable
+  *)
+    echo "Unknown parameter passed: $1"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+# Check specified profile
+if [ -z "$profile" ]; then
+  echo -e "No profile specified, use one of: [cut.it, infra]"
+  exit 1
+fi
+
+cd docker/"$profile" || exit
 docker-compose down
-cd ../scripts || exit
+cd ../../scripts || exit
