194794b0e2f247bb6956e7895986d5dfd8780dff
AvaPL <241574@student.pwr.edu.pl>
2020-10-18 23:07:27 +0200

    Fixed failing KafkaConnectorTest.

--- common/links/src/test/scala/links/kafka/KafkaConnectorTest.scala
+++ common/links/src/test/scala/links/kafka/KafkaConnectorTest.scala
@@ -2,7 +2,8 @@ package links.kafka
 
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Flow, Sink, Source}
-import com.dimafeng.testcontainers.{ForAllTestContainer, KafkaContainer}
+import com.dimafeng.testcontainers.KafkaContainer
+import com.dimafeng.testcontainers.scalatest.TestContainerForAll
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
@@ -13,14 +14,14 @@ import scala.concurrent.duration._
 class KafkaConnectorTest
     extends AnyWordSpec
     with Matchers
-    with ForAllTestContainer {
+    with TestContainerForAll {
   implicit val system: ActorSystem = ActorSystem("test")
 
-  override def container: KafkaContainer = KafkaContainer()
+  override val containerDef: KafkaContainer.Def = KafkaContainer.Def()
 
   "KafkaConnector" when {
     "given message is sent to producer" should {
-      "receive sent message in consumer" in {
+      "receive sent message in consumer" in withContainers { container =>
         val kafkaConfig          = KafkaConfig(container.bootstrapServers)
         val kafkaConnector       = KafkaConnector(kafkaConfig)
         val topic                = "testTopic"
