8b2ee040a7a498f4a2b711193819fc1f6927d338
Paweł Cembaluk <241574@student.pwr.edu.pl>
2020-10-21 22:06:57 +0200

    Cleaned up cut link flow test.

--- integration-tests/src/test/scala/CutLinkSpec.scala
+++ integration-tests/src/test/scala/CutLinkSpec.scala
@@ -1,5 +1,7 @@
+import akka.actor.Scheduler
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.pattern.retry
 import cats.data.EitherT
 import cats.implicits._
 import com.dimafeng.testcontainers.lifecycle._
@@ -42,50 +44,82 @@ class CutLinkSpec
     "a cut link request is sent" should {
       "send link to Elasticsearch" in withContainers {
         case kafka and elasticsearch =>
-          // TODO: Cleanup
-          val kafkaConfig     = KafkaConfig(kafka.bootstrapServers)
-          val kafkaConnector  = KafkaConnector(kafkaConfig)
-          val linkMessageFlow = LinkMessageFlow(kafkaConnector)
-          val linkService     = LinkService(linkMessageFlow)
-          val graphQl         = GraphQl(linkService)
-          val bulkConfig      = BulkConfig(1000, 10.seconds)
-          val elasticConfig =
-            ElasticConfig(elasticsearch.httpHostAddress, bulkConfig)
-          val elasticConnector = ElasticConnector(elasticConfig)
+          val kafkaConnector   = testKafkaConnector(kafka)
+          val graphQl          = testGraphQl(kafkaConnector)
+          val elasticConnector = testElasticConnector(elasticsearch)
           SaveLinkFlow(kafkaConnector, elasticConnector)
           val uri = "http://test.com"
-          val mutation =
-            s"""{
-               | "query": "mutation($$uri: String!) { cutLink(uri: $$uri) { id } }",
-               | "variables": {
-               |   "uri": "$uri"
-               | }
-               |}
-               |""".stripMargin
-          val httpEntity = HttpEntity(ContentTypes.`application/json`, mutation)
-          val id = Post("/graphql", httpEntity) ~> graphQl.route ~> check {
-            status should be(StatusCodes.OK)
-            responseAs[Json].hcursor
-              .downField("data")
-              .downField("cutLink")
-              .downField("id")
-              .as[String]
-              .toOption
-              .get
-          }
-          val linkFuture = EitherT(
-            akka.pattern.retry(
-              () =>
-                elasticConnector
-                  .getDocument(Index.linkStoreIndex, id)
-                  .map(doc => decode[Link](doc)),
-              10,
-              1.second
-            )(system.dispatcher, system.scheduler)
-          ).rethrowT
-          val link = Await.result(linkFuture, 10.seconds)
-          link.uri should be(uri)
+
+          val createdLinkId = sendGraphQlQuery(graphQl, uri)
+          val retrievedLink =
+            retrieveEsLinkWithRetries(elasticConnector, createdLinkId)
+
+          retrievedLink.uri should be(uri)
       }
     }
   }
+
+  private def testKafkaConnector(kafka: KafkaContainer) = {
+    val kafkaConfig = KafkaConfig(kafka.bootstrapServers)
+    KafkaConnector(kafkaConfig)
+  }
+
+  private def testElasticConnector(elasticsearch: ElasticsearchContainer) = {
+    val bulkConfig    = BulkConfig(1000, 10.seconds)
+    val elasticConfig = ElasticConfig(elasticsearch.httpHostAddress, bulkConfig)
+    ElasticConnector(elasticConfig)
+  }
+
+  private def testGraphQl(kafkaConnector: KafkaConnector) = {
+    val linkMessageFlow = LinkMessageFlow(kafkaConnector)
+    val linkService     = LinkService(linkMessageFlow)
+    GraphQl(linkService)
+  }
+
+  private def sendGraphQlQuery(graphQl: GraphQl, uri: String) = {
+    val mutation   = cutLinkMutation(uri)
+    val httpEntity = HttpEntity(ContentTypes.`application/json`, mutation)
+    Post("/graphql", httpEntity) ~> graphQl.route ~> check {
+      status should be(StatusCodes.OK)
+      returnLinkId
+    }
+  }
+
+  private def cutLinkMutation(uri: String) =
+    s"""{
+       | "query": "mutation($$uri: String!) { cutLink(uri: $$uri) { id } }",
+       | "variables": {
+       |   "uri": "$uri"
+       | }
+       |}
+       |""".stripMargin
+
+  private def returnLinkId =
+    responseAs[Json].hcursor
+      .downField("data")
+      .downField("cutLink")
+      .downField("id")
+      .as[String]
+      .toOption
+      .get
+
+  private def retrieveEsLinkWithRetries(
+      elasticConnector: ElasticConnector,
+      documentId: String
+  ) = {
+    implicit val scheduler: Scheduler = system.scheduler
+    val linkFuture = EitherT(
+      retry(() => retrieveEsLink(elasticConnector, documentId), 10, 1.second)
+    ).rethrowT
+    val retrievedLink = Await.result(linkFuture, 10.seconds)
+    retrievedLink
+  }
+
+  private def retrieveEsLink(
+      elasticConnector: ElasticConnector,
+      documentId: String
+  ) =
+    elasticConnector
+      .getDocument(Index.linkStoreIndex, documentId)
+      .map(doc => decode[Link](doc))
 }
