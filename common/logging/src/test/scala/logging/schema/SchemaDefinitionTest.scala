8ce36c2b4ba9c93c611b8b8b26b5de9802db6124
AvaPL <241574@student.pwr.edu.pl>
2020-10-26 13:24:33 +0100

    Added changeLevels mutation tests, rearranged tests code.

--- common/logging/src/test/scala/logging/schema/SchemaDefinitionTest.scala
+++ common/logging/src/test/scala/logging/schema/SchemaDefinitionTest.scala
@@ -4,8 +4,9 @@ import akka.actor.ActorSystem
 import io.circe.Json
 import io.circe.parser.parse
 import logging.filter.ServiceFilter
-import logging.model.levels.Levels
+import logging.model.levels.{Levels, LevelsChange}
 import logging.service.LoggingService
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import sangria.ast.Document
@@ -19,27 +20,24 @@ import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 
-class SchemaDefinitionTest extends AnyWordSpec with Matchers {
+class SchemaDefinitionTest
+    extends AnyWordSpec
+    with Matchers
+    with BeforeAndAfterEach {
   implicit val system: ActorSystem = ActorSystem("test")
 
   val servicePackage                       = "service.test"
   val schema: Schema[LoggingService, Unit] = SchemaDefinition.schema
+  val initialLevels: Levels                = Levels(service = Info, libraries = Info)
   val loggingService: LoggingService =
-    LoggingService(servicePackage, Levels(service = Warn, libraries = Debug))
+    LoggingService(servicePackage, initialLevels)
+
+  override protected def beforeEach(): Unit =
+    loggingService.changeLevels(initialLevels)
 
   "SchemaDefinition" when {
     "receives levels query" should {
       "return current logging levels" in {
-        val levelsQuery =
-          graphql"""
-            query {
-              levels {
-                service
-                libraries
-              }
-            }
-          """
-
         val queryFuture = query(levelsQuery).map(parseData(_, "levels"))
         val result      = Await.result(queryFuture, 10.seconds)
 
@@ -50,37 +48,29 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
 
     "receives changeLevels mutation" should {
       "change both levels" in {
-        val levels = Levels(service = Trace, libraries = Error)
-        val mutation =
-          graphql"""
-            mutation($$levelsChangeInput: LevelsChangeInput!) {
-              changeLevels(levelsChangeInput: $$levelsChangeInput) {
-                service
-                libraries
-              }
-            }
-          """
-        val variables = levelsVariables(levels)
+        val targetLevels = Levels(service = Trace, libraries = Error)
+        val variables    = levelsChangeInput(levelsJson(targetLevels))
 
-        val queryFuture =
-          query(mutation, variables).map(parseData(_, "changeLevels"))
-        val result = Await.result(queryFuture, 10.seconds)
-
-        result should be(levelsJson(levels))
-        checkServiceFilter(levels)
+        changeLevelsAndCheck(variables, targetLevels)
       }
 
       "change only service level" in {
-        // TODO: Add test
+        val levelsChange   = LevelsChange(service = Some(Debug))
+        val variables      = levelsChangeInput(levelsChangeJson(levelsChange))
+        val expectedLevels = loggingService.currentLevels.copy(service = Debug)
+
+        changeLevelsAndCheck(variables, expectedLevels)
       }
 
       "change only libraries level" in {
-        // TODO: Add test
+        val levelsChange   = LevelsChange(libraries = Some(Warn))
+        val variables      = levelsChangeInput(levelsChangeJson(levelsChange))
+        val expectedLevels = loggingService.currentLevels.copy(libraries = Warn)
+
+        changeLevelsAndCheck(variables, expectedLevels)
       }
     }
   }
-
-  // TODO: Change methods order
 
   private def query(document: Document, variables: Json = Json.obj()) =
     Executor.execute(
@@ -90,8 +80,21 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
       userContext = loggingService
     )
 
-  private def levelsVariables(levels: Levels) =
-    Json.obj("levelsChangeInput" -> levelsJson(levels))
+  private def levelsQuery =
+    graphql"""
+            query {
+              levels {
+                service
+                libraries
+              }
+            }
+          """
+
+  private def parseData(json: Json, field: String) =
+    json.hcursor.downField("data").get[Json](field) match {
+      case Right(value)  => value
+      case Left(failure) => sys.error(failure.message)
+    }
 
   private def levelsJson(levels: Levels) =
     json(s"""|{
@@ -106,11 +109,28 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
       case Left(failure)    => throw failure
     }
 
-  private def parseData(json: Json, field: String) =
-    json.hcursor.downField("data").get[Json](field) match {
-      case Right(value)  => value
-      case Left(failure) => sys.error(failure.message)
-    }
+  private def levelsChangeInput(json: Json) =
+    Json.obj("levelsChangeInput" -> json)
+
+  private def changeLevelsAndCheck(variables: Json, expectedLevels: Levels) = {
+    val queryFuture = query(changeLevelsMutation, variables).map(
+      parseData(_, "changeLevels")
+    )
+    val result = Await.result(queryFuture, 10.seconds)
+
+    result should be(levelsJson(expectedLevels))
+    checkServiceFilter(expectedLevels)
+  }
+
+  private def changeLevelsMutation =
+    graphql"""
+            mutation($$levelsChangeInput: LevelsChangeInput!) {
+              changeLevels(levelsChangeInput: $$levelsChangeInput) {
+                service
+                libraries
+              }
+            }
+          """
 
   private def checkServiceFilter(levels: Levels) = {
     val serviceFilter =
@@ -121,4 +141,11 @@ class SchemaDefinitionTest extends AnyWordSpec with Matchers {
     serviceFilter.servicePackage should be(servicePackage)
     serviceFilter.levels should be(levels)
   }
+
+  private def levelsChangeJson(levelsChange: LevelsChange) =
+    json(s"""|{
+             |  ${levelsChange.service.map(service => s""""service": "${service.name}"""").getOrElse("")}
+             |  ${levelsChange.libraries.map(libraries => s""""libraries": "${libraries.name}"""").getOrElse("")}
+             |}
+             |""".stripMargin)
 }
